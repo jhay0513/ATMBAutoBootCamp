bad5fa713350a58861e03126b13cb664bf18866c
Jason Eligio <jason.eligio@anytimemailbox.com>
2025-03-25 15:00:31 +0800

    Added Module 3 Materials

---
+++ Demo Codes/Module 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+class Program
+{
+    static void Main()
+    {
+        // Create Edge WebDriver
+        IWebDriver driver = new EdgeDriver();
+
+        // Navigate to URL
+        driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+
+        // Find Full Name input field
+        IWebElement fullNameInput = driver.FindElement(By.Id("userName"));
+
+        // Find Submit button
+        IWebElement submitButton = driver.FindElement(By.XPath("//button[text()='Submit']"));
+
+        // Type Full Name input field
+        fullNameInput.SendKeys("Jason Eligio");
+
+        // Click Submit button
+        submitButton.Click();
+
+        // Wait for 5 seconds
+        Thread.Sleep(5000);
+
+        // Close the browser
+        driver.Quit();
+    }
+}
