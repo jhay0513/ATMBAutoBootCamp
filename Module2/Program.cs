addb885fc9cd3469b38620e9c2f132b4714549bb
Jason Eligio <jason.eligio@anytimemailbox.com>
2025-03-25 11:34:15 +0800

    added Module 2 demo codes

---
+++ Module2/Program.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+class Program
+{
+    static void Main()
+    {
+        IWebDriver driver = new EdgeDriver();
+        driver.Navigate().GoToUrl("https://atmbqa1.wpenginepowered.com/");
+        driver.Quit();
+    }
+}
